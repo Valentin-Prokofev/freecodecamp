4d9fb01daa4730de1983c50af19ef063db691ca7
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-02-16 18:57:03 +0300

    change task104 and 105, add task106-110

---
+++ task107/app.js
@@ -0,0 +1,6 @@
+function convertToInteger(str) {
+    const res = parseInt(str);
+    return res;
+    }
+
+    convertToInteger("56");
---
+++ task108/app.js
@@ -0,0 +1,6 @@
+function convertToInteger(str) {
+    const res = parseInt(str, 2);
+    return res;
+   }
+
+   convertToInteger("10011");
